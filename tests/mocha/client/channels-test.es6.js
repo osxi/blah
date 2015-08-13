326519a31e5ce6221d6e918b4251514271a0e12f
osxi <osxi@users.noreply.github.com>
2015-08-13 00:57:30 -0500

    add test for default app text

--- tests/mocha/client/channels-test.es6.js
+++ tests/mocha/client/channels-test.es6.js
@@ -3,7 +3,7 @@
 if (typeof MochaWeb !== 'undefined') {
   MochaWeb.testOnly(() => {
     describe('channels', () => {
-      it('is ready to be tested', () => {
+      it('can create channels', () => {
         return MTT.clickOn('+ New Channel')
           .then(() => MTT.fillIn('It types something here', 'foo'))
           .then(() => MTT.clickOn('Create'))
@@ -12,6 +12,11 @@ if (typeof MochaWeb !== 'undefined') {
             chai.expect(newChannel.length).to.be.eq(1);
           });
       });
+
+      it('shows Nothing to See Here by default', () => {
+        var defaultText = $("h3:contains('Nothing to See Here')");
+        chai.expect(defaultText.length).to.eql(1);
+      })
     });
   });
 }
