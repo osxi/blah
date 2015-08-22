13eb5cba7be331ee2e7d7622373bb81f943d139a
osxi <osxi@users.noreply.github.com>
2015-08-22 11:07:05 -0500

    add test for sending messages

--- tests/mocha/client/channels-test.es6.js
+++ tests/mocha/client/channels-test.es6.js
@@ -2,18 +2,40 @@
 
 if (typeof MochaWeb !== 'undefined') {
   MochaWeb.testOnly(() => {
+    beforeEach(done => {
+      Messages.insert({
+        userName: 'blahbot',
+        channelName: 'general',
+        body: 'Welcome to #general!'
+      });
+      done();
+    });
+
     describe('channels', () => {
       it('can create channels', () => {
         return MTT.clickOn('+ New Channel')
-          .then(() => MTT.fillIn('It types something here', 'foo'))
-          .then(() => MTT.clickOn('Create'))
+          .then(MTT.fillIn('It types something here', 'foo'))
+          .then(MTT.clickOn('Create'))
           .then(() => {
             var newChannel = $(".side-bar-container a:contains('foo')");
             chai.expect(newChannel.length).to.be.eq(1);
           });
       });
 
-      it.skip('shows Nothing to See Here by default', () => {
+      it('can add new messages', () => {
+        return MTT.fillIn('Say Something!', 'pringles')
+          .then(MTT.clickOn('Send'))
+          .then(() => {
+            var newMessage = Messages.findOne({
+              channelName: 'general',
+              body: 'Welcome to #general!'
+            });
+
+            chai.expect(newMessage).to.not.eql(undefined);
+          });
+      });
+
+      it('shows Nothing to See Here by default', () => {
         var defaultText = $("h3:contains('Nothing to See Here')");
         chai.expect(defaultText.length).to.eql(1);
       })
