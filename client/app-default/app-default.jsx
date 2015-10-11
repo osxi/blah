306c820c81b6478039f224c75ff781bfd36fec90
osxi <osxi@users.noreply.github.com>
2015-10-11 16:16:48 -0500

    remove AppDefault #getInitialState

--- client/app-default/app-default.jsx
+++ client/app-default/app-default.jsx
@@ -3,10 +3,6 @@ AppDefault = React.createClass({
     padding: '10px'
   },
 
-  getInitialState() {
-    return {markdown: 'Loading...'};
-  },
-
   componentDidMount() {
     let url = 'https://raw.githubusercontent.com/osxi/blah/master/README.md';
 
